d82d9afeeeeaa9c3338b26037bcd6541498254ed
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2025-03-22 06:01:27 -0300

    JavaScript Basic - Chapter 01 -- 001

---
+++ first-part/first-charptet/1_basic.js
@@ -0,0 +1,20 @@
+// Basic JavaScript
+
+// Everything after double slashes is a natural language comment.
+// Read the comments carefully: they explain the JavaScript code.
+
+// A variable is a symbolic name for a value.
+// Variables are declared with the var keyword.
+var x;            // Declares a variable named x.
+
+// Values can be assigned to variables with the equal sign.
+x = 0;            // Now the variable x has the value 0.
+console.log(x);   // This writes 0 to the console.
+
+// JavaScript supports several types of values.
+x = 1;            // Numbers.
+x = 0.01;         // Only one Number type for all integers and reals.
+x = "hello world"; // Text strings with double quotes.
+x = 'JavaScript'; // Text strings with single quotes.
+x = true;         // Booleans.
+x = false;        // The other boolean value.
